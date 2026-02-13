627869aeb59be174bbfa16e2ab799234c50b63b1
jasperpaget13 <jasper.paget@ada.ac.uk>
2026-02-13 14:54:35 +0000

    jj

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -1,20 +1,46 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 1.1
-
+SELECT
+member_id,
+first_name,
+last_name,
+email,
+join_date
+FROM members;
 
 -- 1.2
-
+UPDATE members 
+SET phone_number = '07000 10005',
+    email = 'emily.jones.updated@email.com'
+WHERE member_id = 5;
 
 -- 1.3
-
+SELECT COUNT(*) AS total_members
+FROM members;
 
 -- 1.4
-
+SELECT m.member_id AS member_id,
+       m.first_name,
+       m.last_name,
+       COUNT(c.schedule_id) AS registration_count
+FROM members m
+JOIN class_attendance c ON m.member_id = c.member_id
+GROUP BY m.member_id, m.first_name, m.last_name
+ORDER BY registration_count DESC
+LIMIT 1;
 
 -- 1.5
-
+SELECT m.member_id,
+       m.first_name,
+       m.last_name,
+       COUNT(c.schedule_id) AS registration_count
+FROM members m
+LEFT JOIN class_attendance c ON m.member_id = c.member_id
+GROUP BY m.member_id, m.first_name, m.last_name
+ORDER BY registration_count ASC
+LIMIT 1;
 
 -- 1.6
 
