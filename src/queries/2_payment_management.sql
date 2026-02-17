5bc70f35529572b7949b7c8bd09d738fa236f166
jasperpaget13 <jasper.paget@ada.ac.uk>
2026-02-17 16:26:00 +0000

    kkk

--- src/queries/2_payment_management.sql
+++ src/queries/2_payment_management.sql
@@ -1,11 +1,19 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 2.1 
-
+INSERT INTO payments (member_id, amount, payment_date, payment_method, payment_type)
+VALUES (11, 50.00, DATETIME('now'), 'Credit Card', 'Monthly membership fee');
 
 -- 2.2 
 
 
 -- 2.3 
-
+SELECT 
+    payment_id,
+    amount,
+    payment_date,
+    payment_method
+FROM payments
+WHERE payment_type = 'Day pass'
+ORDER BY payment_date;
--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -2,10 +2,21 @@
 .mode column
 
 -- 3.1 
-
+SELECT 
+    equipment_id,
+    name,
+    next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date >= '2025-01-01'
+  AND next_maintenance_date < date('2025-01-01', '+30 days')
+ORDER BY next_maintenance_date;
 
 -- 3.2 
-
+SELECT 
+    type AS equipment_type,
+    COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
 
